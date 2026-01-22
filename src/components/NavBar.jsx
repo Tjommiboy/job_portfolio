09376d4877b702001dcb995c85774fe0954d4bea
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2026-01-22 23:39:14 +0700

    added resume to portfolio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import NavItem from "./NavItem";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaFileAlt } from "react-icons/fa";
 import Calender from "./Calender";
 
 const NavBar = () => {
@@ -82,6 +82,16 @@ const NavBar = () => {
             <NavItem to="/Contact" label="Contact" onClick={closeSidebar} />
           </div>
           <div className="">
+            <a
+              href="https://anandchetty-resume.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={closeSidebar}
+              className="w-[90%] mb-1 flex items-center gap-2  px-2 py-2 bg-sky-400/10 rounded text-amber-50/50 hover:text-blue-700 hover:bg-amber-50/80"
+            >
+              <FaFileAlt className="text-xl" />
+              Resume
+            </a>
             <a
               href="https://www.linkedin.com/in/anand-chetty-0666a8106/"
               target="_blank"
